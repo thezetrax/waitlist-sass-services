7347981965e0313599c88c81f86016ff6961ab8d
Zablon Dawit <zabjd22@gmail.com>
2026-02-27 10:05:05 +0300

    feat(mock-gen): use @waitlist/schema

--- packages/mock-gen/src/index.ts
+++ packages/mock-gen/src/index.ts
@@ -1,3 +1,9 @@
+import type { CreateWaitlist } from "@waitlist/schema";
+
 export const generateMockData = () => {
+  const waitlist: CreateWaitlist = {};
+
+  // Example usage of schema types
   console.log("Generating Test Mock Data!");
+  // console.log(tables, createWaitlist);
 };
