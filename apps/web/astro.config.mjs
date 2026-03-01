b226cdf5dd726e1edd7c93b5c2ae112189c061dc
Zablon Dawit <zabjd22@gmail.com>
2026-03-01 07:12:40 +0300

    feat(web): add react support

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
