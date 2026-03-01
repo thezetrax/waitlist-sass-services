12266a3fb9e9b088dc22f158157ef00ac0baec00
Zablon Dawit <zabjd22@gmail.com>
2026-03-01 11:52:14 +0300

    fix(web): minor ui changes

--- apps/web/src/components/ui/search.tsx
+++ apps/web/src/components/ui/search.tsx
@@ -1,5 +1,13 @@
+import { Button } from "./button";
 import { Input } from "./input";
 
-const SearchComponent = () => <Input placeholder="Search" />;
+const SearchComponent = () => {
+  return (
+    <span className="flex flex-row gap-2 w-full p-24">
+      <Input placeholder="Search" />
+      <Button>Search</Button>
+    </span>
+  );
+};
 
 export { SearchComponent };
