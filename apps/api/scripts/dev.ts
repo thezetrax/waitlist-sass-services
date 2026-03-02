1b9af4b5e6d862b1f49b183d832a39f7a5bd7c6c
Zablon Dawit <zabjd22@gmail.com>
2026-03-02 13:51:25 +0300

    chore(api): update dev build script

--- apps/api/scripts/dev.ts
+++ apps/api/scripts/dev.ts
@@ -6,10 +6,21 @@ const dev = {
   name: "dev-server",
   prefixColor: "green",
 } satisfies ConcurrentlyCommandInput;
+const build = {
+  command: "bun build --watch --target bun --outdir ./dist ./cmd/server.ts",
+  name: "build",
+  prefixColor: "yellow",
+} satisfies ConcurrentlyCommandInput;
 const tsc = {
   command: "tsc",
   name: "tsc --watch",
   prefixColor: "blue",
 } satisfies ConcurrentlyCommandInput;
+const clean = {
+  command: "rm -rf ./dist",
+  name: "clean",
+  prefixColor: "red",
+} satisfies ConcurrentlyCommandInput;
 
-concurrently([dev, tsc]);
+concurrently([clean]);
+concurrently([dev, tsc, build]);
